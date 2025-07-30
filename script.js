70076a6d5df839897df9b55f399e6c9695625b0e
Tofu8278 <shifengtu617@gmail.com>
2025-07-30 18:58:52 +0800

    my brain would want to implode

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-  const cursor = document.querySelector('.custom-cursor');
-  document.addEventListener('mousemove', (e) => {
-    cursor.style.transform = `translate(${e.clientX - 15}px, ${e.clientY - 15}px) rotate(45deg)`;
+const cursor = document.querySelector('.custom-cursor');
+const reticle = document.querySelector('.center-reticle');
+const panels = document.querySelectorAll('.side-box');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  cursor.style.left = `${x - 15}px`;
+  cursor.style.top = `${y - 15}px`;
+
+  reticle.style.left = `${x}px`;
+  reticle.style.top = `${y}px`;
+});
+
+// Cursor + reticle hover effect
+panels.forEach(panel => {
+  panel.addEventListener('mouseenter', () => {
+    cursor.style.borderColor = 'red';
+    reticle.style.borderColor = 'red';
+    reticle.style.animation = 'rotateReticleFast 1s linear infinite';
   });
+  panel.addEventListener('mouseleave', () => {
+    cursor.style.borderColor = '#00ff00';
+    reticle.style.borderColor = '#00ff00';
+    reticle.style.animation = 'rotateReticle 5s linear infinite';
+  });
+});
+
+
+//side menu button section
+const menuToggle = document.getElementById('menuToggle');
+const sidePanel = document.querySelector('.side-panel');
+
+menuToggle.addEventListener('click', () => {
+  sidePanel.classList.toggle('active');
+});
